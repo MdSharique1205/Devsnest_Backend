97308abb8bdb594d93ac09faebe9c570f31df858
Sharique <mdsharique1205@gmail.com>
2021-09-27 16:55:50 +0530

    Completed Day 10

---
+++ Day10/index.js
@@ -0,0 +1,5 @@
+// Amazon Web Services (AWS), the cloud platform offered by Amazon.com
+// AWS is made up of many different cloud computing products and services.
+// The highly profitable division of Amazon provides servers, storage, networking, remote computing, email,
+//  mobile development, and security. AWS can be broken into three main products: EC2, Amazon’s virtual machine service,
+//  Glacier, a low-cost cloud storage service, and S3, Amazon’s storage system.
