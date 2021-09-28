6345387adad188b4f179b7fe7be9311fd883c056
Sharique <mdsharique1205@gmail.com>
2021-09-28 22:17:02 +0530

    Completed Day 11

--- Day11/controllers/register.js
+++ Day11/controllers/register.js
@@ -2,6 +2,31 @@
 // check if email id exists
 // password hashing
 // convert email to lowercase
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
 
-const register = (req, res) => {};
+const saltRounds = 10;
+
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const alreadyExists = await User.findOne({ where: { email } });
+    if (alreadyExists) {
+      res.status(401).send("Email already registered");
+    }
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      email: email.toLowerCase(),
+      password: hash,
+      fullName: "Sharique",
+    });
+    const savedUser = await newUser.save();
+    res.status(201).send("User Registered !");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Something went wrong");
+  }
+};
 module.exports = register;
--- Day11/database/index.js
+++ Day11/database/index.js
@@ -9,3 +9,18 @@ const sequelize = new Sequelize(
       dialect = "postgres"
   }
 );
+
+sequelize.sync();
+
+(async ()=>{
+    try {
+        await sequelize.authenticate();
+        console.log('Connection Established with Database');
+    }
+    catch (err)
+    {
+        console.log('Unable to connect with Database');
+    }
+})();
+
+module.exports = sequelize;
